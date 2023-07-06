1531abfd7c7315e68414edbe96bed9d48883fb92
GhoriHet <ghorihet71@gmail.com>
2023-07-06 13:01:57 +0530

    Updated!.

--- Asset/JS/processer.js
+++ Asset/JS/processer.js
@@ -1,48 +1,61 @@
-let pro_1 = document.getElementById("i3").value
-let pro_2 = document.getElementById("i5").value
-let pro_3 = document.getElementById("i7").value
+// let pro = document.getElementById("Processor");
+// let ram = document.getElementById("ram");
+// let memory = document.getElementById("memory");
+// let gpcard = document.getElementById("gpcard")
+// let total;
 
-let ram_1 = document.getElementById("4gb").value;
-let ram_2 = document.getElementById("8gb").value;
-let ram_3 = document.getElementById("12gb").value;
+// const CalculateCost = (pro) => {
 
-let memory_1 = document.getElementById("256").value;
-let memory_2 = document.getElementById("512").value;
-let memory_3 = document.getElementById("1TB").value;
+//     if (pro === 'i3') {
+//         total =  5000;
+//     }
+//     console.log(total);
 
-let graphic_card1 = document.getElementById("yes").value;
-let graphic_card2 = document.getElementById("no").value;
+//     return false;
+// }
 
-const calculateCost = (gener, ram) => {
 
-    if (gener === 'i3') {
-        return 7000;
-    } else if (gener === 'i5') {
-        return 12000;
-    } else if (gener === 'i7') {
-        return 18000;
+// let total = 0;
+const CalculateCost = () => {
+    // console.log("aaaaaa");
+    let pro = document.getElementById("Processer").value;
+    let ram = document.getElementById("ram").value;
+    let memory = document.getElementById("memory").value;
+    let gpcard = document.getElementById("graphics").value;
+
+    console.log(pro, ram, memory, gpcard);
+
+    if (pro === 'i3') {
+    } else if (ram === '4GB') {
+    } else if (memory === '256GB') {
+    } else if (gpcard === 'yes') {
     }
+    total = 7000 + 3000 + 2500 + 4500;
 
-    if (graphic_card1 === 'y') {
-        return 4500;
-    } else {
-        return 0;
+    if (pro === 'i5') {
+    } else if (ram === '8GB') {
+    } else if (memory === '512GB') {
+    } else if (gpcard === 'yes') {
     }
-    console.log(graphic_card1);
-     return false
-}
-const pcType = (p3, p5, p7, ram4) => {
-    // console.log("asfh");
-    let cost1 = calculateCost(p3);
-    let cost2 = calculateCost(p5);
-    let cost3 = calculateCost(p7);
+    total = 12000 + 6000 + 5000 + 4500;
 
-    let ram1 = calculateCost(ram4);
+    if (pro === 'i7') {
+    } else if (ram === '12GB') {
+    } else if (memory === '1TB') {
+    } else if (gpcard === 'yes') {
+    }
+    total = 18000 + 9000 + 10000 + 4500;
 
-    let msg = p3 + " memory " + cost1 + ' and ' + p5 + " memory " + cost2 + ' and ' + p7 + " memory " + cost3;
+    console.log(total);
 
-    return msg;
+    return false;
 }
 
-let res = pcType('i3', 'i5', 'i7', 'ram');
-console.log(res);
+const pcType = () => {
+
+
+
+
+}
+
+pcType();
