3b42e8b8d8cf5aec0a967f5d9cfe6d5d103b6d63
GhoriHet <ghorihet71@gmail.com>
2023-07-18 10:22:20 +0530

    18-07

---
+++ Asset/JS/product_list.js
@@ -0,0 +1,10 @@
+let arr = [];
+
+const handleSubmit = () => {
+    let product = document.getElementById("proAdd").value
+    // console.log(product);
+
+    arr.push(product)
+
+    return false;
+}
--- Pages/Test-3/todo.js
+++ Pages/Test-3/todo.js
@@ -1,60 +1,51 @@
-let arr = [];
-let update = false;
-let editIndex;
+let allData = [];
+let updateIndex = null;
 
-// Data added.
-const handleinsert = () => {
-    let data = document.getElementById("data").value;
-    arr.push(data);
-
-    display();
-    return false;
-    // console.log('insert', data);
-}
-
-// Data Removed.
-const handleRemove = (i) => {
-    arr.splice(i, 1);
-    // console.log(i);
-    display();
-}
-
-// Data Edited.
-const handleEdit = (i) => {
-    update = true;
-    document.getElementById("data").value = arr[i];
-    // console.log('edit', i);
-    editIndex = i;
-    // console.log(editIndex, 'editIndex');
-}
-
-// Data Display Out.
-const display = () => {
-    let disp = '<ul>';
-
-    arr.map((v, i) => {
-        disp += '<li>' + v;
-        disp += '<button onclick="handleRemove(' + i + ')">  Remove  </button>' + '<button onclick="handleEdit(' + i + ')">  Edit  </button>' + '</li>';
-    })
-    disp += '</ul>';
-    document.getElementById("dispdata").innerHTML = disp;
-}
-// Data Updated.
-const handleupdatedata = () => {
-    // console.log(editIndex, 'handleupdatedata');
-    let data = document.getElementById("data").value;
-    arr[editIndex] = data;
-    // console.log("update final");
-    display();
-}
-
-// Onsubmit Function Called.
 const handleSubmit = () => {
-    if (update) {
-        handleupdatedata();
+    // console.log(fffffffffff);
+
+    let todo = document.getElementById("todo").value;
+    // console.log(todo);
+
+    if (updateIndex !== null) {
+        allData[updateIndex] = todo;
     } else {
-        handleinsert();
+        allData.push(todo);
     }
+    updateIndex = null;
+
+    // console.log(allData);
+    displaydata();
 
     return false;
+}
+
+const handleRemove = (i) => {
+    allData.splice(i, 1);
+    // console.log(allData);
+
+    displaydata();
+
+    return false;
+}
+
+const handleEdit = (i) => {
+    // console.log(allData[i]);
+
+    document.getElementById("todo").value = allData[i];
+    updateIndex = i;
+}
+
+const displaydata = () => {
+    let print = '';
+
+    allData.map((a, i) => {
+
+        print += `<li>${a}`;
+        print += `<button onclick=handleRemove(${i})>Remove</button>`;
+        print += `<button onclick=handleEdit(${i})>Edit</button>`;
+        print += `</li>`;
+    });
+
+    document.getElementById("disp").innerHTML = print;
 }
