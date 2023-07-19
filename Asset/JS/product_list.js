35932baccd2675e94e1ec90839675ea17de01da1
GhoriHet <ghorihet71@gmail.com>
2023-07-19 10:07:18 +0530

    Product List Complete.

--- Asset/JS/product_list.js
+++ Asset/JS/product_list.js
@@ -1,10 +1,50 @@
 let arr = [];
+let updateIndex = null;
 
+// HandleSubmit Value
 const handleSubmit = () => {
-    let product = document.getElementById("proAdd").value
+    // console.log("aaaaaa");
+    let product = document.getElementById("textValue").value
     // console.log(product);
+    if (updateIndex !== null) {
+        arr[updateIndex] = product;
+    } else {
+        arr.push(product);
+    }
+    updateIndex = null;
 
-    arr.push(product)
+    // arr.push(product);
+    // console.log(arr);
+    displayData();
+    return false;
+}
+
+// Data Remove.
+const handleRemove = (i) => {
+    arr.splice(i, 1);
+    // console.log(arr);
+    displayData();
 
     return false;
+}
+
+// Data Edit.
+const handleEdit = (i) => {
+    // console.log(allData[i]);
+
+    document.getElementById("textValue").value = arr[i];
+    updateIndex = i;
+}
+
+// HTML data show.
+const displayData = () => {
+    let print = '';
+
+    arr.map((v, i) => {
+        print += `<li>${v}`;
+        print += `<button onclick=handleRemove(${i})>Remove</button>`;
+        print += `<button onclick=handleEdit(${i})>Edit</button>`;
+        print += `</li>`;
+    });
+    document.getElementById("displayList").innerHTML = print;
 }
