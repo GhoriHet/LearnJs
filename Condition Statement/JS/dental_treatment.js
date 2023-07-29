ae81b7de82b64691e552b0e7dd33f9ad7a1ef2bc
GhoriHet <ghorihet71@gmail.com>
2023-07-29 12:03:47 +0530

    Dental Appoinment Book System!

--- Condition Statement/JS/dental_treatment.js
+++ Condition Statement/JS/dental_treatment.js
@@ -5,6 +5,7 @@ let s4 = 6, c4 = 12000;
 let s5 = 12, c5 = 35000;
 let final_treatment, final_seating, final_costing;
 let allData = [];
+let updateIndex = null
 
 const handleTreatments = (treatments, seating, costing) => {
     document.getElementById('treatmentSelect').innerHTML = treatments;
@@ -20,15 +21,15 @@ const handleTreatments = (treatments, seating, costing) => {
 function Treatments() {
     let treatmentData = document.getElementById('treatment').value;
 
-    if (treatmentData === 'TC') {
+    if (treatmentData === 'Teeth Cleanings') {
         handleTreatments('Teeth Cleanings', s1, c1);
-    } else if (treatmentData === 'FL') {
+    } else if (treatmentData === 'Fillings') {
         handleTreatments('Fillings', s2, c2);
-    } else if (treatmentData === 'RCT') {
+    } else if (treatmentData === 'Root Canal Treatment') {
         handleTreatments('Root Canal Treatment', s3, c3);
-    } else if (treatmentData === 'RC') {
+    } else if (treatmentData === 'RCT + Cover') {
         handleTreatments('RCT + Cover', s4, c4);
-    } else if (treatmentData === 'BI') {
+    } else if (treatmentData === 'Braces/Invisalign') {
         handleTreatments('Braces/Invisalign', s5, c5);
     }
 }
@@ -41,27 +42,43 @@ const handleRemove = (id) => {
     allData.splice(index, 1)   // console.log("allllllvvvvvvvvv", allData);
     DisplayData();
 }
+const handelEdit = (i) => {
+    allData.map((v) => {
+        if (v.id === i) {
+            document.getElementById('treatment').value = v.treatment;
+            document.getElementById("treatmentSelect").innerHTML = v.treatment;
+            document.getElementById("seatingSelect").innerHTML = v.seating;
+            document.getElementById("costingSelect").innerHTML = v.costing;
+            document.getElementById("aptDate").value = v.apt_date;
+        }
+    });
+    let index = allData.findIndex((v) => v.id === i);
+    updateIndex = index
+}
 
 const handleAptDate = () => {
     let getAptDate = document.getElementById('aptDate').value;
     document.getElementById('treatmentsPlan').style.display = 'block';
-
-    let obj = {
-        "id": Math.floor(Math.random() * 1000),
-        "treatment": final_treatment,
-        "seating": final_seating,
-        "costing": final_costing,
-        "apt_date": getAptDate
-    };
-    allData.push(obj);
-
+    if (updateIndex === null) {
+        let obj = {
+            "id": Math.floor(Math.random() * 1000),
+            "treatment": final_treatment,
+            "seating": final_seating,
+            "costing": final_costing,
+            "apt_date": getAptDate
+        };
+        allData.push(obj);
+    } else {
+        allData[updateIndex].apt_date = getAptDate;
+    }
+    updateIndex = null;
     DisplayData();
 }
 
 const DisplayData = () => {
     let data = '';
 
-    data += '<table border="1"> <tr> <th>SrNo.</th> <th>Treatment</th> <th>Seating</th> <th>Costing</th> <th>Action</th> </tr>';
+    data += '<table border="1"> <tr> <th>SrNo.</th> <th>Treatment</th> <th>Seating</th> <th>Costing</th> <th>Remove</th> <th>Edit</th> </tr>';
     allData.map((v) => {
         let newAptDate = new Date(v.apt_date);
         for (SrNo = 1; SrNo <= v.seating; SrNo++) {
@@ -72,6 +89,7 @@ const DisplayData = () => {
                 data += '<td>' + newAptDate.toLocaleDateString() + '</td>';
                 data += '<td>' + Math.round(v.costing / v.seating) + '/- </td>';
                 data += '<td> <button onclick="handleRemove(' + v.id + ')"> <i class="fa-solid fa-trash"></i> </button> </td>'
+                data += '<td> <button onclick="handelEdit(' + v.id + ')"> <i class="fa-regular fa-pen-to-square"></i> </button> </td>'
                 data += '</tr>';
             } else {
                 newAptDate.setDate(newAptDate.getDate() + 7);
@@ -80,6 +98,7 @@ const DisplayData = () => {
                 data += '<td>' + v.treatment + '</td>';
                 data += '<td>' + newAptDate.toLocaleDateString() + '</td>';
                 data += '<td>' + Math.round(v.costing / v.seating) + '/- </td>';
+                data += '<td></td>';
                 data += '<td></td>';
                 data += '</tr>';
             }
